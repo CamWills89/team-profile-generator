24412420f03ecd383ab4803ba0fdb787e2cbf672
AlphaC <cameron.wills89@outlook.com>
2020-09-30 12:03:07 -0400

    added Intern object and passing tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,5 +1,22 @@
-// school
+const Intern = require("../lib/Intern");
+const Employee = require("../lib/Employee");
 
+// school
+test("creates an Intern school name", () => {
+  const intern = new Intern("Cam", 1, "test@test.com", "schoolName");
+
+  expect(intern.school).toBe("schoolName");
+});
 // getSchool()
+test("get school name from getSchool()", () => {
+  const intern = new Intern("Cam", 1, "test@test.com", "schoolName");
+
+  expect(intern.getSchool()).toBe("schoolName");
+});
 
 // getRole() // Overridden to return 'Intern'
+test("get intern role from getRole()", () => {
+  const intern = new Intern("Cam", 1, "test@test.com", "schoolName");
+  //expected to override to return 'Manager'
+  expect(intern.getRole()).toBe("Intern");
+});
