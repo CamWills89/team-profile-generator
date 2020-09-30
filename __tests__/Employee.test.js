ed488e7c9bfd094922f067a79ddf2ac0d9438e18
AlphaC <cameron.wills89@outlook.com>
2020-09-30 14:39:24 -0400

    added basic style sheet

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,15 +3,16 @@ const Employee = require("../lib/Employee");
 test("creates an employee object", () => {
   const employee = new Employee("Cam", 1, "test@test.com");
 
-  expect(employee.name).toBe("Cam");
+  expect(employee.name).toEqual(expect.any(String));
+  expect(employee.name.length).toBeGreaterThan(2);
   expect(employee.id).toEqual(expect.any(Number));
-  expect(employee.email).toEqual(expect.any(String));
+  expect(employee.email).toEqual(expect.stringContaining("@"));
 });
 // getName()
 test("get name from getName()", () => {
   const employee = new Employee("Cam");
 
-  expect(employee.getName()).toBe("Cam");
+  expect(employee.getName()).toEqual(expect.any(String));
 });
 // getId()
 test("get ID from getID()", () => {
@@ -29,7 +30,7 @@ test("get ID from getID()", () => {
 
 // getRole() // Returns 'Employee'
 test("get role from getRole()", () => {
-  const employee = new Employee("Cam", 1, "test@test.com");
+  const employee = new Employee("Cam", 1, "test@test.com", "Employee");
   //expected to return 'Employee'
   expect(employee.getRole()).toBe("Employee");
 });
---
+++ index.js
@@ -0,0 +1,4 @@
+//FS to write the html file
+const fs = require('fs')
+// Inquirer to get user responses
+const inquirer = require('inquirer');
