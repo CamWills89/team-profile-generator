3f835b34941cca87fb3f25eef440e9019dd4c41c
AlphaC <cameron.wills89@outlook.com>
2020-09-30 16:02:25 -0400

    added html template

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 //FS to write the html file
-const fs = require('fs')
+const fs = require("fs");
 // Inquirer to get user responses
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+
+//links to constructor functions
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+//team array to store the user responses in
+const team = []
+
+function userResponse () {
+    return inquirer.prompt({
+    
+})
+}
---
+++ src/page-template.js
@@ -0,0 +1,4 @@
+//icon styles:
+//manager = <h6 class="card-subtitle mb-2 text-white fas fa-mug-hot"> Manager</h6>
+//engineer = <h6 class="card-subtitle mb-2 text-white fas fa-glasses"> Engineer</h6>
+//intern = <h6 class="card-subtitle mb-2 text-white fas fa-user-graduate"> Intern</h6>
